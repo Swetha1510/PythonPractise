c23d89f6b12e88b514c31315a47c8848a2929aac
Swetha1510 <swetha_chinni17@yahoo.com>
2019-07-20 22:33:41 -0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -3,14 +3,14 @@ import json
 from TwitterAPI import TwitterAPI, TwitterConnectionError, TwitterRequestError
 
 kinesis=boto3.client('kinesis',
-                     aws_access_key_id='AKIAI42RFARRUH224INQ',
-                     aws_secret_access_key='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',
+                     aws_access_key_id='xxxxxxxxxxxxxxxxxxxxxx',
+                     aws_secret_access_key='xxxxxxxxxxxxxxxxxxxxxx',
                      region_name='us-east-1'
                      )
 
 twitter=TwitterAPI(
-                   'QRhY8WsSOsfNh5F0RoH2rztob','uzaBmHQiAbDHrr4ofu2WBxolodArz215Ip1YcfHTrccvvxTHK3',
-                   '825228334007148544-Fxo3wfKWbCUBgY0S3RiprexqVerxaop','FovlNX1hMqJNGhpzg6yDJn9fYNVlitk6jOs5fkgNGwrlt'
+                   'xxxxxxxxxx','xxxx',
+                   'xxxxxxxxxx','xxxxxxxxxxxxxxxxxxxxxxxx'
                   )
 
 usersToFollow= [82221567926100480]
